d3500b3311f15aae497c22c770b4893896ab315f
mventa <56406862+mventa@users.noreply.github.com>
2020-04-01 12:22:28 -0400

    Linked navboxes to other pages

--- logically-app/src/Content/Layout/Layout.js
+++ logically-app/src/Content/Layout/Layout.js
@@ -1,29 +1,32 @@
 import React, { Component } from "react";
-import {  Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import "./Layout.module.css";
 
-class Navbar extends Component{
-    render() {
-        return (
-            <div className="nav">
-              <ul id="nav">
-              <li>
-                <Link to="/">Home</Link>
-                  </li>
-                  <li> 
-                   <Link to="/examples">Examples</Link> 
-                    </li> 
-                     <li> 
-                       <Link to="/howto">How to</Link> 
-                          </li> 
-                          <li> 
-                            <Link to="/sketch">New sketch</Link> 
-                               </li> 
-              </ul>
-            </div>
-        );
-    }
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="nav">
+        <ul id="nav">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+ <Link to="/examples">Examples</Link> 
+          </li>
+ 
+          <li>
+ <Link to="/howto">How to</Link> 
+          </li>
+ 
+          <li>
+ <Link to="/sketch">New sketch</Link> 
+          </li>
+ 
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Navbar;
--- logically-app/src/Content/Pages/Home/Home.js
+++ logically-app/src/Content/Pages/Home/Home.js
@@ -1,60 +1,68 @@
 import React, { Component } from "react";
-import {Redirect} from 'react-router-dom';
-import logo from '../../../circuit.svg';
-import styled from 'styled-components';
+import { Redirect, Link } from "react-router-dom";
+import logo from "../../../circuit.svg";
+import styled from "styled-components";
 
 const Navbox = styled.div`height: 150px;   background: red;   justify-content: center; `;
-const NavboxContainer = styled.div`width: 90%;   height: 50%;   padding-right: 5%; padding-left: 5%; grid-column-gap: 25px; background: #F0FFFF;   display: grid; grid-template-columns: 1fr 1fr 1fr `;
+const NavboxContainer = styled.div`
+  width: 90%;
+
+   height: 50%;
+
+   padding-right: 5%;
+  padding-left: 5%;
+  grid-column-gap: 25px;
+  background: #f0ffff;
+
+   display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+`;
 
 class Home extends Component {
-
   state = {
     redirect: false
-  }
+  };
   setRedirect = () => {
     this.setState({
       redirect: true
-    })
-  }
+    });
+  };
   renderRedirect = () => {
     if (this.state.redirect) {
-        this.props.history.push("/howto");
+      this.props.history.push("/howto");
     }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <NavboxContainer>
+          <Link to="/sketch">
+            <Navbox>
+              <div>Create a new sketch</div>
+            </Navbox>
+          </Link>
+          <Link to="/examples">
+            <Navbox>
+              <div>Go to examples</div>
+            </Navbox>
+          </Link>
+          <Link to="/howto">
+            <Navbox>
+              <div>How to use</div>
+            </Navbox>
+          </Link>
+        </NavboxContainer>
+        <div>
+          <img src={logo} className="Logically-logo" alt="logo" />
+          <p>
+            Welcome to the best logic circuit simulator there is,{" "}
+            <code>Logically!</code>
+          </p>
+        </div>
+      </div>
+    );
   }
-
-
-render(){
-
-return(
-  <div className="App">
-    <NavboxContainer>
-      <Navbox>
-        Div 4
-        {this.renderRedirect()}
-        <button onClick={this.setRedirect}>Go to examples</button>
-        </Navbox>
-        <Navbox>
-        Div 4
-        </Navbox>
-        <Navbox>
-        Div 4
-        </Navbox>
-    </NavboxContainer>
-  <div>
-        <img src={logo} className="Logically-logo" alt="logo" />
-  <p>
-    Welcome to the best logic circuit simulator there is, <code>Logically!</code>
-  </p>
-
-
-  </div>
-  </div>
-
-);
-
-
-}
-
 }
 
 export default Home;
