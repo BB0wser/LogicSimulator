b627f3040faa1e06be53c58216bcc0a482c60605
mventa <56406862+mventa@users.noreply.github.com>
2020-04-07 07:11:20 -0400

    Styled navigation bar/list

--- logically-app/src/Content/Layout/Layout.js
+++ logically-app/src/Content/Layout/Layout.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-import "./Layout.module.css";
-
 const Navigation = styled.div`
   position: absolute;
   top: 0;
@@ -12,16 +10,59 @@ const Navigation = styled.div`
   height: 50px;
 
    background: blue;
-  display: flex;
   justify-content: space-between;
+  display: flex;
+  flex-direction: row;
+`;
+
+const BrandLogo = styled.div`
+  width: 200px;
+  background-color: green;
+`;
+
+const NavList = styled.ul`
+  width: 450px;
+
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-around;
+  list-style-type: none;
+  padding-right: 10px;
+
+  li {
+    color: white;
+  }
+`;
+
+const StyledLink = styled(Link)`
+   color: white;
+  text-decoration: none;
+  font-size: 18px;
+  font-weight: bold;
+
+    text-transform: uppercase;
 `;
 
 class Navbar extends Component {
   render() {
     return (
       <Navigation>
-        <div></div>
-        <div></div>
+        <BrandLogo>Logo</BrandLogo>
+        <NavList>
+          <li>
+            <StyledLink to="/">Home</StyledLink>
+          </li>
+          <li>
+             <StyledLink to="/sketch">New sketch</StyledLink> 
+          </li>
+          <li>
+             <StyledLink to="/examples">Examples</StyledLink> 
+          </li>
+          <li>
+             <StyledLink to="/howto">How to</StyledLink> 
+          </li>
+        </NavList>
       </Navigation>
     );
   }
