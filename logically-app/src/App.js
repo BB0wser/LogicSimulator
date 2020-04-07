83ecf2b7771a92a0529a63c8b1f9d78f602375cf
mventa <56406862+mventa@users.noreply.github.com>
2020-04-07 06:48:21 -0400

    Styled the navbar
    Added color, added spaces, deleted comments in App

--- logically-app/src/App.js
+++ logically-app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 //import Toolbar from "./Toolbar/Toolbar";
 import Home from "./Content/Pages/Home/Home";
@@ -6,35 +6,30 @@ import Examples from "./Content/Pages/Examples/Examples";
 import HowTo from "./Content/Pages/HowTo/HowTo";
 import Navbar from "./Content/Layout/Layout";
 import SketchBoard from "./Content/Pages/SketchBoard/SketchBoard";
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
 
-/*
-class App extends Component {
-render() {
-  return (
-    <div>
-      <Router>
-            <Switch>
-              <Route path="/" exact component={Home} />
-            </Switch>
-      </Router>
-    </div>
-  );
-}
-}
-*/
+const Container = styled.div`
+  position: absolute;
+  top: 50px;
+  left: 0;
+  width: 100%;
+  height: 100% - 50px;
+`;
 
 function App() {
   return (
     <div>
       <Router>
-        <Navbar/>
-            <Switch>
-              <Route path="/" exact component={Home} />
-              <Route path="/examples" exact component={Examples} />
-              <Route path="/howto" exact component={HowTo} />
-              <Route path="/sketch" exact component={SketchBoard} />
-            </Switch>
+        <Navbar />
+        <Container>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/examples" exact component={Examples} />
+            <Route path="/howto" exact component={HowTo} />
+            <Route path="/sketch" exact component={SketchBoard} />
+          </Switch>
+        </Container>
       </Router>
     </div>
   );
--- logically-app/src/Content/Layout/Layout.js
+++ logically-app/src/Content/Layout/Layout.js
@@ -1,55 +1,30 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 import "./Layout.module.css";
+
+const Navigation = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+
+   background: blue;
+  display: flex;
+  justify-content: space-between;
+`;
 
 class Navbar extends Component {
   render() {
     return (
-      <div className="nav">
-        <ul id="nav">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-             <Link to="/examples">Examples</Link> 
-          </li>
-          <li>
-             <Link to="/howto">How to</Link> 
-          </li>
-          <li>
-             <Link to="/sketch">New sketch</Link> 
-          </li>
-           
-        </ul>
-      </div>
+      <Navigation>
+        <div></div>
+        <div></div>
+      </Navigation>
     );
   }
 }
 
 export default Navbar;
-
-/*
-const toolbarStyle = {
-  display: 'flex',
-  width: '1000px',
-  backgroundColor: '#d6e2ea',
-  padding: '6px 8px'
-};
-
-class Toolbar extends Component {
-  render() {
-    return (
-      <div style={toolbarStyle}>
-        <button>Home</button>
-        <button>examples</button>
-        <button>How-to</button>
-        <div style={{ flex: 1 }}></div>
-        <button>Sketch</button>
-      </div>
-    );
-  }
-}
-
-export default Toolbar;
-*/
