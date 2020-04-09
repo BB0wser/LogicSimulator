967b90707789232a2697387fc377e7620082930c
mventa <56406862+mventa@users.noreply.github.com>
2020-04-09 09:37:26 -0400

    python backend moved into a folder, deleted some unnecessary files

--- python/serv.py
+++ python/serv.py
@@ -10,19 +10,19 @@ def home():
 @app.route("/jsonex", methods=['POST'])
 def jasonex():
 	rquest = request.get_json()
-	
+
 	lang = rquest['language']
 	pversion = rquest['version_info']['python']
 	frame = rquest['framework']
 	fversion = rquest['version_info']['flask']
 	type3 = rquest['types'][2]
 	boolean = rquest['boolean_test']
-	
+
 	return '''The language is {} version {}
 The framework is {} version {}
 The third type is {}
 The boolean was {}'''.format(lang, pversion, frame, fversion, type3, boolean)
-		
+
 
 if __name__ == "__main__":
-	app.run(debug=True, port=5000)	
+	app.run(debug=True, port=5000)
