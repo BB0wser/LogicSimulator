3db289275fb20ed3e0b7a9b4224e7e21bc951c2f
mventa <56406862+mventa@users.noreply.github.com>
2020-04-10 13:16:22 -0400

    Connected!
    I am quitting computer science I spent 2hours on this and it was a simple true or false (and a little more) ... ... ... :-/

--- python/serv.py
+++ python/serv.py
@@ -1,34 +1,33 @@
 from flask import Flask, request
-#from flask_cors import CORS
+from flask_cors import CORS
 import json
 import sys
 
 app = Flask(__name__)
+CORS(app)
+#print(sys.path)
 
-print(sys.path)
-
-@app.route("/", methods=['POST', 'GET'])
+@app.route("/", methods=['POST', 'GET', 'OPTIONS'])
+#@crossorigin(origin='localhost:3000',headers=['Content- Type','Authorization'])
 def home():
-#	response.headers.add('Access-Control-Allow-Origin', '*')
+	#response.headers.add('Access-Control-Allow-Origin', '*')
 	return 'what you looking at?'
 
 @app.route("/jsonex", methods=['POST'])
 def jasonex():
 	rquest = request.get_json()
-	
-	gates = rquest['gates']
-	connections = rquest['connections']
-	inputs = rquest['inputs']
-	outputgate = rquest['outputgate']
-	
-	#call function here with these to run through logic
-	#return boolean output in that function
-	#output = backend(gates,connections, inputs, outputgate)
-	
-	#for now just returns this
-	output = inputs[0] and inputs[1]
-	rv = {'output' : output}
-	return json.dumps(rv)
+
+	lang = rquest['language']
+	pversion = rquest['version_info']['python']
+	frame = rquest['framework']
+	fversion = rquest['version_info']['flask']
+	type3 = rquest['types'][2]
+	boolean = rquest['boolean_test']
+
+	return '''The language is {} version {}
+The framework is {} version {}
+The third type is {}
+The boolean was {}'''.format(lang, pversion, frame, fversion, type3, boolean)
 
 
 if __name__ == "__main__":
