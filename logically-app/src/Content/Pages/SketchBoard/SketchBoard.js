88a9bf4dfaf2cfabec19b2366f0d942da7512e9b
mventa <56406862+mventa@users.noreply.github.com>
2020-04-21 12:34:25 -0400

    Small fix

--- logically-app/src/Content/Pages/SketchBoard/SketchBoard.js
+++ logically-app/src/Content/Pages/SketchBoard/SketchBoard.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom"; /*important libry*/
 import { Stage, Layer, Rect, Text } from "react-konva";
 import Konva from "konva";
-import Example from "../../../UI/Button/Example";
 import logo from "../../../circuit.svg";
 import and from "../../../Logos/and.svg";
 import styled from "styled-components";
